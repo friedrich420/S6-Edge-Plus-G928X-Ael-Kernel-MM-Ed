887c9e420776c99865ae2004a3ea2f29d849aeab
HRTKernel <support@hacker-kernel.com>
2016-03-05 09:33:30 +0100

    cpufreq: add interextrem

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -467,6 +467,9 @@ extern struct cpufreq_governor cpufreq_gov_intellimm;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WAVE)
 extern struct cpufreq_governor cpufreq_gov_wave;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_wave)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTEREXTREM)
+extern struct cpufreq_governor cpufreq_gov_interextrem;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interextrem)
 #endif
 
 
